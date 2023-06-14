454b37776a14678e71ee53cb00030ff3f5ede1c3
Shruthaja <mail.shruthaja@gmail.com>
2023-06-14 14:25:00 -0500

    2 queries done

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import request
 from flask import Flask
 from flask import render_template
+from flask_cdn import CDN
+cdn = CDN()
 import pyodbc
 import os
 from azure.storage.blob import BlobClient, BlobServiceClient
@@ -23,16 +25,28 @@ cursor = conn.cursor()
 # which tells the application which URL should call
 # the associated function.
 print(cursor)
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
+    result=[]
     if request.method=='POST':
         query="select * from dbo.earthquake where dbo.earthquake.mag>5.0"
-    return render_template("index.html")
-
-
-# main driver function
+        cursor.execute(query)
+        result=cursor.fetchall()
+    return render_template("index.html",result=result)
+@app.route('/page2.html',methods=['GET','POST'])
+def page2():
+    result=[]
+    if request.method=="POST":
+        mag=request.form['mag']
+        ranje=request.form['range']
+        ranje=int(ranje)
+        ranje=-1*ranje
+        latest_date='2023-06-11T19:11:07.423Z'
+        query="select * from dbo.earthquake WHERE dbo.earthquake.time >= DATEADD(DAY , ? , '2023-06-11T19:11:07.423Z') AND time <= '2023-06-11T19:11:07.423Z' and dbo.earthquake.mag=?;"
+        cursor.execute(query,ranje,mag)
+        result=cursor.fetchall()
+    return render_template("page2.html",result=result)
 if __name__ == '__main__':
-    # run() method of Flask class runs the application
-    # on the local development server.
+    cdn.init_app(app)
     app.run(debug=True)
